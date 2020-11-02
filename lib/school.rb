bd2f4fc7f344a38acdc80cef457861c6e35970d4
zaynegoering <zaynegoering@gmail.com>
2020-11-02 11:46:55 -0800

    commit tree d02e986d18ff83c2f9e2cb518c31c9d7afec6263

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require "pry"
+
+class School
+    
+    def initialize(school)
+        @school = school
+        @roster = {}
+        
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student, grade)
+        @roster[grade] ||= []
+        @roster[grade] << student
+        # binding.pry
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted = {}
+        @roster.each do |grade, students|
+            sorted[grade] = students.sort
+        end
+        sorted
+    end
+end
